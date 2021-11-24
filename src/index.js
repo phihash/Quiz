c333705c6b63dfa4b968c2d6730e60fecbe779af
phihash <insharp0220@gmail.com>
2021-11-24 10:59:14 +0900

    問題数を選んで、問題を表示するようにしている途中

--- src/index.js
+++ src/index.js
@@ -18,42 +18,70 @@
 //   playFalseSound();
 // })
 // でーた
-const questions = {
-    "ALS":["筋萎縮性側索硬化症","多発性硬化症 あるいは 僧帽弁狭窄症","再生不良性貧血","原発性胆汁性胆管炎","慢性閉塞性肺疾患"],
-    "PSP" : ["進行性核上性麻痺"],
-    "CK":["クレアチニンキナーゼ"],
-    "HA":["溶血性貧血"],
-    "PNH":["発作性夜間ヘモグロビン尿症"],
-    "AIHA":["自己免疫性溶血性貧血"],
-    "HL":["Hodgkinリンパ腫"],
-    "PCP":["ニューモシスチス肺炎"],
-    "DM":["糖尿病"],
-    "IVR":["画像下治療"],
-    "AD":["アルツハイマー病 あるいは 常染色体優性遺伝"],
-    "AAA":["腹部大動脈瘤","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"],
-    "VF":["心室細動"],
-    "AF":["心房細動"],
-    "MS":["多発性硬化症 あるいは 僧帽弁狭窄症"],
-    "AA":["再生不良性貧血","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"],
-    "PBC":["原発性胆汁性胆管炎","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"],
-    "COPD":["慢性閉塞性肺疾患","筋萎縮性側索硬化症","心房細動","原発性胆汁性胆管炎","溶血性貧血"]
-  
-};
+const questions = [
+  ["ALSとは?",["筋萎縮性側索硬化症","多発性硬化症 あるいは 僧帽弁狭窄症","再生不良性貧血","原発性胆汁性胆管炎","慢性閉塞性肺疾患"]],
+  ["PSPとは?",["進行性核上性麻痺","多発性硬化症 あるいは 僧帽弁狭窄症","再生不良性貧血","原発性胆汁性胆管炎","慢性閉塞性肺疾患"]],
+  [
+    "MSとは?",["多発性硬化症 あるいは 僧帽弁狭窄症","onn","再生不良性貧血","原発性胆汁性胆管炎","慢性閉塞性肺疾患"]
+  ],
+  [
+    "AAとは?",["再生不良性貧血","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"]
+  ],
+  [
+    "AAAとは?",["腹部大動脈瘤","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"]
+  ],
+  [
+    "CKとは?",["クレアチンキナーゼ","アセチルコリン","ドパミン","ノルアドレナリン","ネオン"]
+  ],
+  [
+    "PBCとは?",["原発性胆汁性胆管炎","筋萎縮性側索硬化症","進行性核上性麻痺","クレアチニンキナーゼ","溶血性貧血"]
+  ],
+  [
+    "COPDとは?",["慢性閉塞性肺疾患","筋萎縮性側索硬化症","心房細動","原発性胆汁性胆管炎","溶血性貧血"]
+  ],
+  ["AFとは?",["心房細動","慢性閉塞性肺疾患","筋萎縮性側索硬化症","原発性胆汁性胆管炎","溶血性貧血"]]
+];
+
 // ここまでデータ
 const candidate = document.getElementById("candidate");
 const sentence = document.getElementById("sentence");
 const changeButton = document.getElementById("changeButton");
 
-function choiceQuestionRandomly(){
-  const num =  Math.floor(Math.random() * Object.keys(questions).length);
-  console.log(num);
-  return num;
+function questionQuantity(){
+  for(let i=0;i < 5;i++){
+    if(document.querySelectorAll("input[type=radio]")[i].checked){
+      return  parseInt(document.querySelectorAll("input[type=radio]")[i].value);
+    }
+  }
 }
 
-// 書き換え
-function rewrite(){
-  sentence.textContent = "";
-  sentence.textContent = choiceQuestionRandomly();
+function choiceQuestionSetRandomly(){
+  let num = questionQuantity();
+  let questionset = [];
+  for(let i=0;i < num;i++){
+    let questionIndex =  Math.floor(Math.random() * questions.length);
+    questionset.push(questions[questionIndex]);
+  }
+  return questionset
+}
+
+function displayQuestionSet(){
+  let questionset = choiceQuestionSetRandomly();
+  let num = questionset.length;
+  removeCandidate();
+  sentence.textContent = questionset[0][0];
+  for(let i =0;i < 5;i++){
+    let elmDiv = document.createElement("div");
+    let elmInput = document.createElement('input');
+    console.log(questionset[0][1]);
+    elmInput.type = "radio";
+    elmInput.name = "choices";
+    elmInput.value = questionset[0][1][i];
+    console.log(elmInput);
+    elmInput.textContent = questionset[0][1][i];
+    candidate.appendChild(elmDiv);
+    elmDiv.appendChild(elmInput);
+  }
 }
 
 function displayCandidate(){
@@ -68,9 +96,12 @@ function removeCandidate(){
     candidate.removeChild( candidate.firstChild );
   }
 }
-function processCandidate(){
-  removeCandidate();
-  displayCandidate();
-}
-changeButton.addEventListener('click',processCandidate);
+
+
+changeButton.addEventListener('click',displayQuestionSet
+// () => {
+//   changeButton.textContent = "次へ";
+//   sentence.textContent = choiceQuestionSetRandomly();
+// }
+);
 
